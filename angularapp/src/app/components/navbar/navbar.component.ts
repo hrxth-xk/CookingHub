b68630e638c96c60f710e576585f9325bd6ccdc3
Hrithik Reddy Chenreddy <122119750+hrxth-xk@users.noreply.github.com>
2026-04-18 18:47:04 +0000

    navbar component updated for mobile responsiveness.

--- angularapp/src/app/components/navbar/navbar.component.ts
+++ angularapp/src/app/components/navbar/navbar.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  mobileMenuOpen = false;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.mobileMenuOpen = false;
   }
 
 }
