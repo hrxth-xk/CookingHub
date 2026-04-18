0449e6670c08e1876d36845363dde06c7edb3651
Hrithik Reddy Chenreddy <122119750+hrxth-xk@users.noreply.github.com>
2026-04-18 18:52:07 +0000

    admin nav and user nav components updated for mobile responsiveness.

--- angularapp/src/app/components/usernav/usernav.component.ts
+++ angularapp/src/app/components/usernav/usernav.component.ts
@@ -9,12 +9,22 @@ import { AuthService } from '../../services/auth.service';
 })
 export class UsernavComponent {
   user: any;
+  mobileMenuOpen = false;
  
   constructor(private auth: AuthService, private router: Router) {
     this.auth.currentUser.subscribe(u => this.user = u);
   }
+
+  toggleMobileMenu(): void {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.mobileMenuOpen = false;
+  }
  
   logout() {
+    this.closeMobileMenu();
     if (confirm('Are you sure you want to logout?')) {
       this.auth.logout();
       this.router.navigate(['/home']);
